7c661a287f285331738f78422d474b07ffb8746e
n0808 <life.with.chums.119@gmail.com>
2020-09-27 23:02:55 +0900

    add　新規登録
    ユーザー管理するための機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get 'users', to: 'users/registrations#new'
+  end
+
 end
