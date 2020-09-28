6a6d56004028762028d62177a9b95d779cf22adc
n0808 <life.with.chums.119@gmail.com>
2020-09-28 17:11:50 +0900

    追加：バリデーションの設定
    バリデーションの設定

--- app/models/hairdresser.rb
+++ app/models/hairdresser.rb
@@ -4,4 +4,13 @@ class Hairdresser < ApplicationRecord
   validates_associated :images
   validates :images, presence: { message: "は一枚以上必須です" }
 
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :explain, presence: true
+  validates :area, presence: true
+  validates :prefecture_id, presence: true
+  validates :gender_id, presence: true
+  validates :technique_id, presence: true
+  validates :history_id, presence: true
+  
 end
